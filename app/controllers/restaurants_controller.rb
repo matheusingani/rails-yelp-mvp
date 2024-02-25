0e39776d7dfd63547760e1c3be0a427a018a6bb1
matheus ingani <matheusingani@hotmail.com>
2024-02-25 12:25:28 +0000

    Restaurants reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,35 @@
 class RestaurantsController < ApplicationController
-
   def index
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
 
+  def new
+    @restaurant = Restaurant.new
+  end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+    def destroy
+      @restaurant = Restaurant.find(params[:id])
+      @task.destroy
+      redirect_to restaurant_path
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,34 @@ Restaurant.destroy_all
 Restaurant.create(
   name: 'Trattoria Toscana',
   address: 'Rua Italia, 123',
+  phone_number: '(11) 5555-5555',
   category: 'Italian'
 )
 
 Restaurant.create(
   name: 'Le Bistro Français',
   address: 'Avenue des Champs-Élysées, 456',
+  phone_number: '(33) 1234-5678',
   category: 'French'
 )
 
 Restaurant.create(
   name: 'Sushi House',
   address: 'Rua do Sushi, 789',
+  phone_number: '(22) 9876-5432',
   category: 'Japonese'
 )
 
 Restaurant.create(
   name: 'Taqueria Belgian',
   address: 'Avenida de los Muertos, 321',
+  phone_number: '(55) 9999-8888',
   category: 'Belgian'
 )
 
 Restaurant.create(
   name: 'Steakhouse Grill',
   address: 'Broadway Avenue, 101',
+  phone_number: '(1) 212-555-1212',
   category: 'Chinese'
 )
